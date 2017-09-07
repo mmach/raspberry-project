655c9b34d77f6730e2d1f14058e6395ed7508681
Michal Mach <m.mach.90@gmail.com>
2017-09-07 12:07:03 +0200

    websockety

--- server.js
+++ server.js
@@ -1,8 +1,33 @@
 const Koa = require('koa');
-const app = new Koa();
+const router = require('koa-router');
 
-app.use(async ctx => {
-  ctx.body = 'Hello World';
+
+const app = new Koa();
+const socket = require('koa-websocket')(app);
+// Loading router package
+
+
+
+const http = router();
+const ws = router();
+var initFunc=function(ctx){
+
+    return ctx;
+}
+http.get('/',     async (ctx) => {
+    console.log('test');
+    return await initFunc(ctx);
 });
 
+ws.get('/socketunction', async (ctx) => {
+    console.log('test');
+    ctx.websocket.send('Hello World');
+    ctx.websocket.on('message', function(message) {
+      // do something with the message from client
+          console.log(message);
+    });
+ 
+});
+app.use(http.routes())
+app.ws.use(ws.routes())
 app.listen(3000);
